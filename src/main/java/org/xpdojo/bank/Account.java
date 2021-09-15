2d09ab557c4b89226440a84c4eeb2b40f6fe734b
Max <max@svayav.pro>
2021-09-15 14:55:43 +0300

    deposit, multiple deposits

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,13 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int amount;
+
+    public void deposit(final int value) {
+        amount += value;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,12 +4,31 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
 
+    Account subject;
+
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void accountStartsEmpty() {
+        subject = new Account();
+        assertEquals(0, subject.getAmount());
     }
+
+    @Test
+    public void depositAnAmount() {
+        subject = new Account();
+        subject.deposit(100);
+        assertEquals(100, subject.getAmount());
+    }
+
+    @Test
+    public void depositMultipleAmounts() {
+        subject = new Account();
+        subject.deposit(100);
+        subject.deposit(100);
+        assertEquals(200, subject.getAmount());
+    }
+
 }
